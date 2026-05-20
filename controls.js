66166a76b9c23d771e160f6e3429b0fb8a860138
unknown <chinmaylokhande30@gmail.com>
2026-05-20 19:24:12 +0530

    first commit

---
+++ controls.js
@@ -0,0 +1,62 @@
+class Controls {
+    constructor(type){
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.down = false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyListeners();
+                break;
+            case "DUMMY":
+                this.forward = true;
+                break;
+        }
+
+    }
+
+
+    #addKeyListeners(){
+        document.onkeydown = (event) =>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = true
+                    break;
+                case "ArrowRight":
+                    this.right = true
+                    break;
+                case "ArrowUp":
+                    this.forward = true
+                    break;
+                case "ArrowDown":
+                    this.down = true
+                    break;
+          
+                    
+            }
+           
+        }
+
+        document.onkeyup = (event) =>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = false
+                    break;
+                case "ArrowRight":
+                    this.right = false
+                    break;
+                case "ArrowUp":
+                    this.forward = false
+                    break;
+                case "ArrowDown":
+                    this.down = false
+                    break;
+               
+            }
+           
+        }
+    }
+
+}
+
